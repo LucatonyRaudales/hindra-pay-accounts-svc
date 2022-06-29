600a903b61a6535c280e854753e48c469dbd756f
Tony R. Raudales <tony.raudales@scrummers.co>
2022-06-29 06:56:08 -0600

    adding all resources, only i need to fix some things

---
+++ api/controllers/routes.go
@@ -0,0 +1,20 @@
+package controllers
+
+import "github.com/victorsteven/fullstack/api/middlewares"
+
+func (s *Server) initializeRoutes() {
+
+	// Home Route
+	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
+
+	// Login Route
+	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
+ter.HandleFunc("/users/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteUser)).Methods("DELETE")
+
+	//Posts routes
+	s.Router.HandleFunc("/posts", middlewares.SetMiddlewareJSON(s.CreatePost)).Methods("POST")
+	s.Router.HandleFunc("/posts", middlewares.SetMiddlewareJSON(s.GetPosts)).Methods("GET")
+	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJSON(s.GetPost)).Methods("GET")
+	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdatePost))).Methods("PUT")
+	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareAuthentication(s.DeletePost)).Methods("DELETE")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/LucatonyRaudales/hindra-pay-solana-accounts-svc/api"
+
+func main() {
+	api.Run()
+}
